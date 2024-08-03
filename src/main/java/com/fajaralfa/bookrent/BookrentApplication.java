1437145b3f71cea5fb81190b63c981e3fa5128ec
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-08-03 07:14:17 +0700

    initial commit

---
+++ src/main/java/com/fajaralfa/bookrent/BookrentApplication.java
@@ -0,0 +1,13 @@
+package com.fajaralfa.bookrent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fajaralfa/bookrent/BookrentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fajaralfa.bookrent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookrentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
