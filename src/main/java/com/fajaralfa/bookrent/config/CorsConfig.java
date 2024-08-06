e89f354b0ff0388ddecbf9bf07bc7d800865017e
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-08-06 20:01:52 +0700

    + spring boot security & @EnableWebSecurity CorsConfig

--- src/main/java/com/fajaralfa/bookrent/config/CorsConfig.java
+++ src/main/java/com/fajaralfa/bookrent/config/CorsConfig.java
@@ -4,12 +4,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
 import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-//@EnableWebSecurity
+@EnableWebSecurity
 public class CorsConfig {
 
     @Bean
