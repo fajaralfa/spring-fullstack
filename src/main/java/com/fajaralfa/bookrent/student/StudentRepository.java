1d8d363aa3dbc39df85f6bb600ed4efce7845334
fajaralfa <101091656+fajaralfa@users.noreply.github.com>
2024-08-07 13:25:39 +0700

    remove unused import

--- src/main/java/com/fajaralfa/bookrent/student/StudentRepository.java
+++ src/main/java/com/fajaralfa/bookrent/student/StudentRepository.java
@@ -2,7 +2,6 @@ package com.fajaralfa.bookrent.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestController
 public interface StudentRepository extends JpaRepository<Student, Integer> {
